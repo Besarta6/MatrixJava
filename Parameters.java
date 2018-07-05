af9a3ccf9107c1c537b2d7f4c33bf9a46470a010
Besarta6 <40863835+Besarta6@users.noreply.github.com>
2018-07-05 16:48:03 +0200

    Add files via upload

---
+++ Parameters.java
@@ -0,0 +1,19 @@
+ 
+public class Parameters {
+    // madhesia e popullates 10 dhe 50 eksperimenti 1
+public int popsize = 50;   
+// numri i turit 10 dhe 50 eksperimenti 2
+public int tournamentSize=10;
+public double bestValue;
+
+// 500, 1000 2000, 5000 - eksperimenti 3 
+public int[][][] answer ;
+public int iterations =1000; 
+// gjasa per mutation 0.1 dhe 0.5 eksperimeti 4
+public double probMutation = 0.5;
+// gjasa per crossover 0.5 dhe 1.0 eksperimenti 5   
+public double probCrossover = 1;
+
+public long start;    
+public long time;    
+}
